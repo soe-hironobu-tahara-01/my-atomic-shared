12222e5b9a7e65dce38209ae373459b5c66b957f
soe-hironobu-tahara-01 <hironobu.tahara.wt@s1.nttdocomo.com>
2025-12-04 20:20:39 +0900

    Create index.ts

---
+++ src/components/organisms/index.ts
@@ -0,0 +1,5 @@
+export { UserForm } from './UserForm';
+export type { UserFormProps, UserFormValues } from './UserForm';
+
+export { UserCard } from './UserCard';
+export type { UserCardProps } from './UserCard';
